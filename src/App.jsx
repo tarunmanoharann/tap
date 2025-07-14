d81d1d1c5e9cf56e4ed3c55789bd25d37f97ec2b
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2025-07-15 00:01:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import BillTracker from "./components/BillTracker"
-
+import BillTracker from "./components/BillTracker";
+import ProjectRationale from "./components/ProjectRationale";
 
 function App() {
-  
-
   return (
-    <>
-       <BillTracker/>
-    </>
-  )
+    <div className="min-h-screen bg-gray-50">
+      <BillTracker />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <ProjectRationale />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
